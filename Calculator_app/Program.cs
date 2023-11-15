fc49e444fa2851578e2d0641e2d412f4e566223a
Mehmetufuk1 <150259221+Mehmetufuk1@users.noreply.github.com>
2023-11-15 22:32:27 +0300

    Add project files.

---
+++ Calculator_app/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Calculator_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2;
+            Console.WriteLine("Basit Bir C# hesap makinesi");
+            Console.WriteLine("işlem seçiniz [+],[-],[/],[*]");
+            string islem = (Console.ReadLine());
+        yeniIslem:
+            try
+            {
+                Console.WriteLine("Birinci Sayıyı giriniz");
+                sayi1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("İkinci Sayıyı Giriniz");
+                sayi2 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception hata)
+            {
+                Console.WriteLine(hata.Message);
+                goto yeniIslem;
+            }
+
+
+            if (sayi2 != 0)
+
+            {
+
+                switch (islem)
+                {
+                    case "+":
+                        Console.WriteLine("Toplama" + (sayi1 + sayi2));
+                        break;
+                    case "-":
+                        Console.WriteLine("çıkarma: " + (sayi1 - sayi2));
+                        break;
+                    case "/":
+                        Console.WriteLine("Bölme İşlemi: " + (sayi1 / sayi2));
+                        break;
+                    case "":
+                        Console.WriteLine("Çarpma İşlemi " + (sayi1 * sayi2));
+                        break;
+                    default:
+                        Console.WriteLine("işlem yapılamadı");
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hatalı Giriş yaptınız");
+                goto yeniIslem;
+            }
+
+        }
+    }
+}
